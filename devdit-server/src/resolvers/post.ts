1f696529b2fd1e2c7216d1a19de6110bbdfb331b
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-03-13 16:21:20 +0530

    update graphql code for update and create post

--- devdit-server/src/resolvers/post.ts
+++ devdit-server/src/resolvers/post.ts
@@ -1,6 +1,6 @@
 import { Post } from '../entities/Post';
 import { MyContext } from 'src/types';
-import { Arg, Ctx, Int, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from 'type-graphql';
 
 @Resolver()
 export class PostResolver {
@@ -11,9 +11,39 @@ export class PostResolver {
 
   @Query(() => Post, { nullable: true})
   post(
-    @Arg('id', () => Int) id: number,
+    @Arg('id') id: number,
     @Ctx() { em }: MyContext
   ): Promise<Post | null> {
     return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title') title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title })
+    await em.persistAndFlush(post)
+    return post
+  }
+
+  @Mutation(() => Post, { nullable: true })
+  async updatePost(
+    @Arg('id') id: number,
+    @Arg('title', () => String, { nullable: true }) title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    const post = await em.findOne(Post, { id })
+
+    if(!post) {
+      return null
+    }
+
+    if(typeof title !== 'undefined') {
+      post.title = title
+      await em.persistAndFlush(post)
+    }
+    
+    return post
   }
 }
