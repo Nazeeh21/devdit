e7b1b33b734f6ffa83f863aa1da68d724a6be6ea
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-03-13 11:01:55 +0530

    Change project structure

--- devdit-server/dist/index.js
+++ devdit-server/dist/index.js
@@ -18,8 +18,8 @@ const mikro_orm_config_1 = __importDefault(require("./mikro-orm.config"));
 const main = () => __awaiter(void 0, void 0, void 0, function* () {
     const orm = yield core_1.MikroORM.init(mikro_orm_config_1.default);
     yield orm.getMigrator().up();
-    const post = orm.em.create(Post_1.Post, { title: 'my first post' });
-    yield orm.em.persistAndFlush(post);
+    const posts = yield orm.em.find(Post_1.Post, {});
+    console.log(posts);
 });
 main().catch((e) => {
     console.log(e);
--- devdit-server/src/index.ts
+++ devdit-server/src/index.ts
@@ -6,8 +6,10 @@ import microConfig from './mikro-orm.config'
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
   await orm.getMigrator().up()
-  const post = orm.em.create(Post, { title: 'my first post' });
-  await orm.em.persistAndFlush(post);
+  // const post = orm.em.create(Post, { title: 'my first post' });
+  // await orm.em.persistAndFlush(post);
+  const posts = await orm.em.find(Post, {})
+  console.log(posts)
 };
 
 main().catch((e) => {
