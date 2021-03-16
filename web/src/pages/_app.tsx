6d914893b5e38db1c2b0172129c6979c7c0f2402
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-03-16 12:58:42 +0530

    Fix build errors

--- web/src/pages/_app.tsx
+++ web/src/pages/_app.tsx
@@ -6,7 +6,7 @@ import theme from '../theme'
 const client = createClient({ url: 'http://localhost:4000/graphql', fetchOptions: {
   credentials: 'include'
 }})
-function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }: any) {
   return (
     <Provider value={client}>
       <ChakraProvider resetCSS theme={theme}>
