9d12c9a4a22d74d5190b90501242ef6dae6e947f
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-03-16 12:28:23 +0530

    Add login route

---
+++ web/src/pages/login.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { Formik, Form } from 'formik';
+import Wrapper from '../components/Wrapper';
+import { InputField } from '../components/InputField';
+import { Box } from '@chakra-ui/layout';
+import { Button } from '@chakra-ui/button';
+import { useLoginMutation } from '../generated/graphql';
+import { toErrorMap } from '../utils/toErrorMap';
+import { useRouter } from 'next/router';
+
+const Login : React.FC<{}> = ({}) => {
+  const router = useRouter();
+
+  const [, login] = useLoginMutation();
+  return (
+    <Wrapper variant='small'>
+      <Formik
+        initialValues={{ username: '', password: '' }}
+        onSubmit={async (values, { setErrors }) => {
+          const response = await login({ options: values});
+
+          if (response.data?.login.errors) {
+            console.log('error response in register user : ', response.data);
+            setErrors(toErrorMap(response.data.login.errors));
+            // syntax we are getting from backend [{field: 'username', message: 'something wrong'}]
+            // setErrors({
+            //   username: 'usernamee already taken'
+            // })
+          } else if (response.data?.login.user) {
+            // worked
+            router.push('/');
+          }
+          // console.log(values);
+        }}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <InputField
+              name='username'
+              placeholder='username'
+              label='Username'
+            />
+            <Box mt={4}>
+              <InputField
+                name='password'
+                placeholder='password'
+                label='Password'
+                type='password'
+              />
+            </Box>
+            <Button
+              mt={4}
+              type='submit'
+              isLoading={isSubmitting}
+              colorScheme='teal'
+            >
+              Login
+            </Button>
+          </Form>
+        )}
+      </Formik>
+    </Wrapper>
+  );
+};
+
+export default Login;
