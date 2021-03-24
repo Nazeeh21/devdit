8b6e52c82a543beceaada51a712adbfd9eea990c
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-03-24 19:46:07 +0530

    Add code in backend to hide other person's email while fetching posts

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -4,10 +4,12 @@ import {
   Arg,
   Ctx,
   Field,
+  FieldResolver,
   Mutation,
   ObjectType,
   Query,
   Resolver,
+  Root,
 } from 'type-graphql';
 import argon2 from 'argon2';
 import { COOKIE_NAME, FORGET_PASSWORD_PREFIX } from '../constants';
@@ -34,8 +36,19 @@ class UserResponse {
   user?: User;
 }
 
-@Resolver()
+@Resolver(User)
 export class UserResolver {
+  @FieldResolver(() => String)
+  email(@Root() user: User, @Ctx() { req }: MyContext) {
+    if (req.session.userId === user.id) {
+      // this is the current user and it is okay to show them their own email
+      return user.email;
+    }
+
+    // current user wants to see someone else's email
+    return '';
+  }
+
   @Mutation(() => UserResponse)
   async changePassword(
     @Arg('token') token: string,
