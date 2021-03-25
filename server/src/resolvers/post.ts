6dda1d2284acca7cc9852c2dc84c2772e6bd7a28
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-03-25 16:16:50 +0530

    Add functionality to downdoot the updooted post and updoot the downdoooted post

--- server/src/resolvers/post.ts
+++ server/src/resolvers/post.ts
@@ -53,24 +53,46 @@ export class PostResolver {
     const realValue = isUpdoot ? 1 : -1;
     const { userId } = req.session;
 
-    // await Updoot.insert({
-    //   userId,
-    //   postId,
-    //   value: realValue,
-    // });
+    const updoot = await Updoot.findOne({ where: { postId, userId } });
 
-    await getConnection().query(`
-      START TRANSACTION;
+    if (updoot && updoot.value !== realValue) {
+      // the user has voted on the post before and they are changing their vote
 
-      insert into updoot ("userId", "postId", value)
-      values (${userId}, ${postId}, ${realValue});
+      await getConnection().transaction(async tm => {
+        await tm.query(`
+          update updoot 
+          set value = $1
+          where "postId" = $2 and "userId" = $3
+        `, [realValue, postId, userId])
 
-      update post
-      set points = points + ${realValue}
-      where id =  ${postId};
-      COMMIT;
-    `,)
-    
+        await tm.query(
+          `
+        update post
+      set points = points + $1
+      where id =  $2`,
+          [2 * realValue, postId]
+      })
+    } else if (!updoot) {
+      // has never voted before
+      await getConnection().transaction(async (tm) => {
+        await tm.query(
+          `
+        insert into updoot ("userId", "postId", value)
+      values ($1, $2, $3)
+        `,
+          [userId, postId, realValue]
+        );
+
+        await tm.query(
+          `
+        update post
+      set points = points + $1
+      where id =  $2`,
+          [realValue, postId]
+        );
+      });
+    }
+
     return true;
   }
 
