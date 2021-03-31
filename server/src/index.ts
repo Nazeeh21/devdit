9bd9fd91688904c8405c0ae99dc4ebdca5a9afe3
Nazeeh2000 <nazeehvahora.786@gmail.com>
2021-03-31 16:21:40 +0530

    change domain in index.ts of server

--- server/src/index.ts
+++ server/src/index.ts
@@ -62,7 +62,7 @@ const main = async () => {
         httpOnly: true,
         sameSite: 'lax', //csrf
         secure: __prod__, //cookie only works in https
-        domain: __prod__ ? '.codeponder.com' : undefined,
+        domain: __prod__ ? '.com' : undefined,
       },
       saveUninitialized: false,
       secret: process.env.SESSION_SECRET,
