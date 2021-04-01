46e7df08bcd42fdbdf361569012fec06554b5919
Nazeeh Vahora <nazeehvahora.786@gmail.com>
2021-04-01 14:47:37 +0530

    Add .env.local file in web directory

--- web/src/utils/createUrqlClient.ts
+++ web/src/utils/createUrqlClient.ts
@@ -105,7 +105,7 @@ export const createUrqlClient = (ssrExchange: any, ctx: any) => {
     cookie = ctx?.req?.headers?.cookie;
   }
   return {
-    url: process.env.NEXT_PUBLIC_API_URL,
+    url: process.env.NEXT_PUBLIC_API_URL as string,
     fetchOptions: {
       credentials: 'include' as const,
       headers: cookie
